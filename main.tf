6f879e07e76ae3d43259e14c61938a82bbe7a974
theppasiva <theppasiva@gmail.com>
2024-09-11 23:25:36 +0530

    updated main.tf and variable.tf

--- main.tf
+++ main.tf
@@ -4,4 +4,14 @@ module "roboshop" {
     environment = var.environment
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
+
+#public subnet
+public_subnets_cidr = var.public_subnets_cidr
+
+#private subnet
+private_subnets_cidr = var.private_subnets_cidr
+
+#private subnet
+database_subnets_cidr = var.database_subnets_cidr
+
 }
--- variables.tf
+++ variables.tf
@@ -19,3 +19,14 @@ variable "environment" {
     default = "dev"
   
 }
+variable "public_subnets_cidr" {
+    default = ["10.0.1.0/24","10.0.2.0/24"]
+  
+}
+variable "private_subnets_cidr" {
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "database_subnets_cidr" {
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
